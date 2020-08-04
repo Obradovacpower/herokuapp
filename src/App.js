1d142ca0fe68a0d501b53dac33e055b348e1e1e6
Ermin <obradovacpower@gmail.com>
2020-08-04 20:56:51 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          "This is it MUAHAHAHAHAH"
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>The New Auctionhouse is here xDD</code>.
+
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=92iSIvPt4QQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hits for pimps 2020 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
